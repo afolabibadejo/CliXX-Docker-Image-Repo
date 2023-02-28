171bab14058bff688ed1d83067d04bab2ffd03bf
afolabibadejo <116470486+afolabibadejo@users.noreply.github.com>
2023-02-28 16:09:54 -0500

    updating image

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,9 @@ define( 'DB_USER', 'wordpressuser' );
 define( 'DB_PASSWORD', 'W3lcome123' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'wordpressdbclixxjenkins.ccj9m3bhtkw7.us-east-1.rds.amazonaws.com' );
+define( 'DB_HOST', 'wordpressdbclixxecs.cuvq1ns8yo5h.us-east-1.rds.amazonaws.com' );
+##define( 'DB_HOST', 'wordpressdbclixxjenkins.ccj9m3bhtkw7.us-east-1.rds.amazonaws.com' );
+####@@wp.config old endpoint
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
